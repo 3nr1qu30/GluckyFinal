9165c913ed8901cbabb7b9d9076da42e7c6b79da
Marco <117793899+marcoBartolo55@users.noreply.github.com>
2023-04-17 19:27:15 -0600

    No se

--- Glucky/src/app/routers/Doctores.js
+++ Glucky/src/app/routers/Doctores.js
@@ -4,11 +4,12 @@ const DoctoresControllers = require('../controllers/DoctoresControllers');
 //rutas get
 router.get('/Dashboard',DoctoresControllers.dashboardDoctores);
 router.get('/Peticiones',DoctoresControllers.peticionesDoctor);
-router.get('/Citas',DoctoresControllers.peticionesCita);
+router.get('/ConsultaPacienteDash',DoctoresControllers.detallesPaciente);
 
+
+//rutas post
 router.post('/CitasAcepta',DoctoresControllers.citasDoctorAcepta);
 router.post('/CitasDeclina',DoctoresControllers.citasDoctorDeclina);
-
 router.post('/Peticiones',DoctoresControllers.peticionesDoctorAcepta);
 router.post('/Peticion',DoctoresControllers.peticionesDoctorDeclina);
 
