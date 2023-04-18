7d28a2e8cdaedf8c4c8d87dd038740cf3588d4c5
Sardipondi69 <gsandovalv2001@alumno.ipn.mx>
2023-04-17 21:31:56 -0600

    va

--- Glucky/src/app/public/js/plantillaAlertas.js
+++ Glucky/src/app/public/js/plantillaAlertas.js
@@ -1,11 +1,3 @@
-function rellenoDatos() {
-  Swal.fire({
-    icon: 'error',
-    title: 'Error',
-    text: 'Por favor, rellene bien los datos',
-  });
-}
-
 function verificarDoctor(e) {
   Swal.fire({
     icon: 'error',
