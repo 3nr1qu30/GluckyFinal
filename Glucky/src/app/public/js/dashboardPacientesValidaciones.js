c355e2fa134c641a00ba372234d802bee0f3c4c0
Sardipondi69 <gsandovalv2001@alumno.ipn.mx>
2023-05-03 01:35:54 -0600

    se acabo la validacion de horaas de mierda

--- Glucky/src/app/public/js/dashboardPacientesValidaciones.js
+++ Glucky/src/app/public/js/dashboardPacientesValidaciones.js
@@ -1,8 +1,8 @@
 const formularioNuevaCita = document.getElementById("formularioNuevaCita");
 const fechaActual1 = new Date();
 const horaActual = fechaActual1.getHours();
-const fechaIngresada = document.getElementById('glucosaNormal').value;
-const horaIngresada = document.getElementById('glucosaNormal2').value;
+var fechaIngresada = document.getElementById('glucosaNormal').value;
+var horaIngresada = document.getElementById('glucosaNormal2').value;
 const inputs = document.querySelectorAll('#formularioNuevaCita input');
 const campos = {fecha: false, hora: false}
 var mes = (fechaActual1.getMonth() + 1).toString();
@@ -13,41 +13,48 @@ var fechaActual2 = fechaActual1.getFullYear() + "-" + mes + "-" + dia;
 var fechaMaxima = (fechaActual1.getFullYear() + 1) + "-" + mes + "-" + dia;
 
 const validarFechayHora = (e) => {
-  console.log("El nombre:" + e.target.name);
   switch(e.target.name){
     case "FechaForm":
+      fechaIngresada = e.target.value
       console.log(fechaIngresada);
-      if (fechaActual2 < fechaIngresada) {
-        console.log(`La fecha ingresada no es válida`);
-        campos.fecha = false;
-        console.log("La fecha dlv1:" + e.target.value);
-      } else if (e.target.value === ""){
-        console.log(`La fecha ingresada no es válida`);
-        campos.fecha = false;
-        console.log("La fecha dlv2:" + e.target.value);
-        console.log(fechaActual2 < fechaIngresada);
-      } else{
-        console.log(`La fecha esta bien pa`);
-        console.log("La fecha buena:" + e.target.value);
-        console.log(`Fecha actual: ${fechaActual2}`);
-        console.log(`Fecha ingresada: ${fechaIngresada}`);
-        console.log(fechaActual2 < fechaIngresada);
+      if (fechaActual2 < fechaIngresada && fechaIngresada < fechaMaxima) {
         campos.fecha = true;
+      } else if (e.target.value === ""){
+        campos.fecha = false;
+        swal({
+          icon: "error",
+          title: "Sin fecha",
+          text: "No has ingresado alguna fecha"
+        });
+      }else{
+        campos.fecha = false;
+        swal({
+          icon: "error",
+          title: "Fecha extemporánea",
+          text: "La fecha no puede ser mayor a un año"
+        });
       }
+      
       break;
       case "HoraForm":
-        if (horaIngresada < '07:00' || horaIngresada > '21:00') {
-          console.log(`La hora ingresada no está dentro del rango permitido`);
-          campos.hora = false;
-          console.log("La hora dlv1:" + e.target.value);
+        horaIngresada = e.target.value
+        if (horaIngresada > '07:00' && horaIngresada < '21:00') {
+          campos.hora = true;
         } else if (e.target.value === ""){
-          console.log(`La hora ingresada no está dentro del rango permitido`);
+          console.log(`No hay hora we`);
           campos.hora = false;
-          console.log("La hora dlv2:" + e.target.value);
+          swal({
+            icon: "error",
+            title: "Sin hora",
+            text: "No has ingresado una hora"
+          });
         } else{
-        console.log(`La hora ingresada esta bien`);
-        campos.hora = true;
-        console.log("La hora buena:" + e.target.value);
+          campos.hora = false;
+          swal({
+            icon: "error",
+            title: "Horario inactivo",
+            text: "El doctor no está disponible en ese horario"
+          });
       }
       break;
   }
@@ -62,26 +69,12 @@ inputs.forEach((input) => {
 formularioNuevaCita.addEventListener('submit', (e) => {
   e.preventDefault();
   if (campos.fecha && campos.hora) {
-    console.log(`Si se envio wtf`);
     formularioNuevaCita.submit()
   } else {
-  console.log(`No se puede cabrona`);
+  swal({
+    icon: "error",
+    title: "Datos erroneos",
+    text: "Por favor, ingresa datos válidos"
+  });
 }
 });
-
-
-/* if (!fechaIngresada || fechaIngresada < fechaActual || fechaIngresada > new Date(fechaActual.getFullYear() + 1, fechaActual.getMonth(), fechaActual.getDate())) {
-  alert('La fecha ingresada no es válida');
-}
-if (horaIngresada < '07:00' || horaIngresada > '21:00') {
-  alert('La hora ingresada no está dentro del rango permitido');
-}
-document.querySelector('.glucosaNormalForm2').addEventListener('submit', function (e) {
-  if (fechaIngresada && fechaIngresada >= fechaActual && fechaIngresada <= new Date(fechaActual.getFullYear() + 1, fechaActual.getMonth(), fechaActual.getDate()) && horaIngresada >= '07:00' && horaIngresada <= '21:00') {
-    // Enviar formulario
-  } else {
-    e.preventDefault();
-  }
-});
-
- */
