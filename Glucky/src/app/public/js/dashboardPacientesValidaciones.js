ad141571a4b17d9505f0bd00ce0a53317e9df19a
Sardipondi69 <gsandovalv2001@alumno.ipn.mx>
2023-05-03 00:14:42 -0600

    pendejada aun no acabada de validar fechas

--- Glucky/src/app/public/js/dashboardPacientesValidaciones.js
+++ Glucky/src/app/public/js/dashboardPacientesValidaciones.js
@@ -1,56 +1,87 @@
-const formulario = document.getElementById("formulario");
-const inputs = document.querySelectorAll('#formulario input');
-const expReg = {
-    valorGlucosa: /saas/,
-    valorPresionSistolica: /saas/,
-    valorPresionDiastolica: /saas/
-}
-const campos = {
-    valorGlucosa: false,
-    valorPresionSistolica: false,
-    valorPresionDiastolica: false
-}
+const formularioNuevaCita = document.getElementById("formularioNuevaCita");
+const fechaActual1 = new Date();
+const horaActual = fechaActual1.getHours();
+const fechaIngresada = document.getElementById('glucosaNormal').value;
+const horaIngresada = document.getElementById('glucosaNormal2').value;
+const inputs = document.querySelectorAll('#formularioNuevaCita input');
+const campos = {fecha: false, hora: false}
+var mes = (fechaActual1.getMonth() + 1).toString();
+if(mes.length <= 1) mes = "0" + mes;
+var dia = fechaActual1.getDate().toString();
+if(dia.length <= 1) dia = "0" + dia;
+var fechaActual2 = fechaActual1.getFullYear() + "-" + mes + "-" + dia;
+var fechaMaxima = (fechaActual1.getFullYear() + 1) + "-" + mes + "-" + dia;
 
-const validarFormulario = (e) => {
-    console.log(e.target.name);
-    switch(e.target.name){
-        case "glucosa":
-          if(expReg.valorGlucosa.test(e.target.value)){
-            campos.valorGlucosa = true;
-          }
-          else if(e.target.value === ""){
-            campos.valorGlucosa = false;
-          }
-          else{
-            campos.valorGlucosa = false;
-          }
-          break;
-        case "sistolica":
-          if(expReg.valorPresionSistolica.test(e.target.value)){
-            campos.valorPresionSistolica = true;
-          }
-          else if(e.target.value === ""){
-            campos.valorPresionSistolica = false;
-          }
-          else{
-            campos.valorPresionSistolica = false;
-          }
-          break;
-        case "diastolica":
-          if(expReg.valorPresionDiastolica.test(e.target.value)){
-            campos.valorPresionDiastolica = true;
-          }
-          else if(e.target.value === ""){
-            campos.valorPresionDiastolica = false;
-          }
-          else{
-            campos.valorPresionDiastolica = false;
-          }
-          break;
+const validarFechayHora = (e) => {
+  console.log("El nombre:" + e.target.name);
+  switch(e.target.name){
+    case "FechaForm":
+      console.log(fechaIngresada);
+      if (fechaActual2 < fechaIngresada) {
+        console.log(`La fecha ingresada no es válida`);
+        campos.fecha = false;
+        console.log("La fecha dlv1:" + e.target.value);
+      } else if (e.target.value === ""){
+        console.log(`La fecha ingresada no es válida`);
+        campos.fecha = false;
+        console.log("La fecha dlv2:" + e.target.value);
+        console.log(fechaActual2 < fechaIngresada);
+      } else{
+        console.log(`La fecha esta bien pa`);
+        console.log("La fecha buena:" + e.target.value);
+        console.log(`Fecha actual: ${fechaActual2}`);
+        console.log(`Fecha ingresada: ${fechaIngresada}`);
+        console.log(fechaActual2 < fechaIngresada);
+        campos.fecha = true;
       }
+      break;
+      case "HoraForm":
+        if (horaIngresada < '07:00' || horaIngresada > '21:00') {
+          console.log(`La hora ingresada no está dentro del rango permitido`);
+          campos.hora = false;
+          console.log("La hora dlv1:" + e.target.value);
+        } else if (e.target.value === ""){
+          console.log(`La hora ingresada no está dentro del rango permitido`);
+          campos.hora = false;
+          console.log("La hora dlv2:" + e.target.value);
+        } else{
+        console.log(`La hora ingresada esta bien`);
+        campos.hora = true;
+        console.log("La hora buena:" + e.target.value);
+      }
+      break;
+  }
 }
 
 inputs.forEach((input) => {
-    input.addEventListener('keyup', validarFormulario);
-    input.addEventListener('blur', validarFormulario);
+  input.addEventListener('keyup', validarFechayHora);
+  input.addEventListener('blur', validarFechayHora);
 });
+
+
+formularioNuevaCita.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (campos.fecha && campos.hora) {
+    console.log(`Si se envio wtf`);
+    formularioNuevaCita.submit()
+  } else {
+  console.log(`No se puede cabrona`);
+}
+});
+
+
+/* if (!fechaIngresada || fechaIngresada < fechaActual || fechaIngresada > new Date(fechaActual.getFullYear() + 1, fechaActual.getMonth(), fechaActual.getDate())) {
+  alert('La fecha ingresada no es válida');
+}
+if (horaIngresada < '07:00' || horaIngresada > '21:00') {
+  alert('La hora ingresada no está dentro del rango permitido');
+}
+document.querySelector('.glucosaNormalForm2').addEventListener('submit', function (e) {
+  if (fechaIngresada && fechaIngresada >= fechaActual && fechaIngresada <= new Date(fechaActual.getFullYear() + 1, fechaActual.getMonth(), fechaActual.getDate()) && horaIngresada >= '07:00' && horaIngresada <= '21:00') {
+    // Enviar formulario
+  } else {
+    e.preventDefault();
+  }
+});
+
+ */
