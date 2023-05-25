32c360f259ae1e2f8f85ab5466f2fa5aa063f64f
BigSusFG <118015087+BigSusFG@users.noreply.github.com>
2023-05-25 07:57:18 -0600

    ahora si

--- Glucky/src/app/public/js/chatDoctorDes.js
+++ Glucky/src/app/public/js/chatDoctorDes.js
@@ -127,6 +127,7 @@ fetch(url,{
     newMessage.appendChild(messageTime);
     if(i > 0 && formatDate(mensajes[i].date_mensaje) !== formatDate(mensajes[i-1].date_mensaje)){
       const fecha2 = document.createElement('span');
+      fecha2.classList.add('msjdate');
       fecha2.textContent = formatDate(mensajes[i].date_mensaje);
       messagesContainer.appendChild(fecha2);
       
